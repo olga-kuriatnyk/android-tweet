0973bf2464e5c5c01a884d3e4670d1ccb3d7ca09
olga-kuriatnyk <olhak@uw.edu>
2022-03-11 17:21:35 -0800

    showing tweets with RecycleView + relative timestamp

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate.models
 
+import com.codepath.apps.restclienttemplate.TimeFormatter
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -8,6 +9,7 @@ class Tweet {
     var body: String = ""
     var createAt: String = ""
     var user: User? = null
+    var timeStamp: String = ""
 
     companion object {
         fun fromJson(jsonObject: JSONObject) : Tweet {
@@ -15,13 +17,19 @@ class Tweet {
             tweet.body = jsonObject.getString("text")
             tweet.createAt = jsonObject.getString("created_at")
             tweet.user = User.fromJson(jsonObject.getJSONObject("user"))
+            tweet.timeStamp = getFormattedTimestamp(jsonObject.getString("created_at"))  // jsonObject.getString("created_at")
             return tweet
+        }
+
+        fun getFormattedTimestamp(createdAt: String) : String {
+
+            return TimeFormatter.getTimeDifference(createdAt)
         }
 
         fun fromJsonArray(jsonArray: JSONArray): List<Tweet> {
             val tweets = ArrayList<Tweet>()
 
-            for (i in 0..jsonArray.length()) {
+            for (i in 0 until jsonArray.length()) {
                 tweets.add(fromJson(jsonArray.getJSONObject(i)))
             }
             return tweets
